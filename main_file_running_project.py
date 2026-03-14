f22e824162f219911be15dc1b4fdd6b6f02c055b
Timotx23 <thildenbrand@web.de>
2026-03-14 20:02:09 +0100

    best one yet with some SOLID frame work stuff integrated

--- main_file_running_project.py
+++ main_file_running_project.py
@@ -48,8 +48,8 @@ class CoordinateConversions:
         if columns must be changed it does so automatically such that the final average opperation is passed throough successfully
            """
         detecting_columns_to_convert = DetectingColumnsToConvert(self.config)
-        converting_columns = ConvertingColumns(self.config, detecting_columns_to_convert.identify_what_to_convert())
-        converting_columns.converting_column()
+        detecting_columns_to_convert.identify_what_to_convert()
+        detecting_columns_to_convert.detected_column_mismatch_conversion()
         return self.config.access_to_db.groupby(self.config.path_to_reference[0])[self.config.path_to_reference[1]].mean()
 
     def listing_items(self):
@@ -60,6 +60,7 @@ class DetectingColumnsToConvert:
     def __init__(self, config):
         self.config = config
         self.needs_conversion = {x:[i] for i,x in enumerate(self.config.path_to_reference)}
+        self.column_conversion = ConvertingColumns(config)
     def identify_what_to_convert(self):
         """
         This is gpt enhanced version of my code so that it is a bit more functional programming style
@@ -83,23 +84,23 @@ class DetectingColumnsToConvert:
 
         self.needs_conversion = {header: analyze_column(db[header]) for header in headers}
         return self.needs_conversion
-
-
-class ConvertingColumns:
-    def __init__(self, config, dict):
-        self.config = config
-        self.needs_conversion = dict
-    def converting_column(self) -> bool:
+    
+    def detected_column_mismatch_conversion(self) -> bool:
         """This function is in charge of ensuring that columns that have been identified to be in the wrong format be converted into the correct format"""
         for header in self.config.path_to_reference:
             if self.needs_conversion[header][0] == False :
                 if self.needs_conversion[header][1] == float:
-                    if self.convert_to_float(header) == True:
+                    if self.column_conversion.convert_to_float(header) == True:
                         self.needs_conversion[header][0] = True
                     else: raise ValueError("Failed to do some conversion on",self.needs_conversion[header] )
             else:
                 self.needs_conversion[header][0] = True
         return True
+
+
+class ConvertingColumns:
+    def __init__(self, config):
+        self.config = config
     def convert_to_float(self, converting) -> bool:
         """This function will be used to convert columns if needed in order for later opperations
         This function can also run independently from the rest of the class if needed 
@@ -119,10 +120,12 @@ class ConvertingColumns:
         """FOR FUTURE. IGNORE FOR Now"""
         ...
 
-    def collect_items(self):
+
+class Collect_items:
         """
         This function will be used when the user wants to simply collect items from the db(FUTURE)
         """
+        ...
     
     
 
